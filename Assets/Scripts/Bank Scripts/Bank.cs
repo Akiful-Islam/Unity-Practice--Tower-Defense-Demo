b0924af2c629ae9294dd1ebe1433bdebb5a69551
ScorpZer0 <arian.islam024@gmail.com>
2022-10-11 02:45:07 +0600

    Currency UI

--- Assets/Scripts/Bank Scripts/Bank.cs
+++ Assets/Scripts/Bank Scripts/Bank.cs
@@ -1,21 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Bank : MonoBehaviour
 {
     [SerializeField] private int _startingBalance = 150;
+
+    [SerializeField] private TextMeshProUGUI _balanceText;
     private int _currentBalance;
     public int CurrentBalance { get => _currentBalance; }
 
     private void Awake()
     {
         _currentBalance = _startingBalance;
+        UpdateDisplay();
+    }
+
+    private void UpdateDisplay()
+    {
+        _balanceText.text = "Money: " + _currentBalance.ToString();
     }
 
     public void Deposit(int amount)
     {
         _currentBalance += Mathf.Abs(amount);
+        UpdateDisplay();
     }
 
     public void Withdraw(int amount)
@@ -26,5 +37,6 @@ public class Bank : MonoBehaviour
         {
             _currentBalance = 0;
         }
+        UpdateDisplay();
     }
 }
