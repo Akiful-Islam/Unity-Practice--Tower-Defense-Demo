518cb29b5ba4f08aca69079e6d52c52f299482a3
ScorpZer0 <arian.islam024@gmail.com>
2022-10-12 20:16:29 +0600

    Build timer for defenses

--- Assets/Scripts/Defense Scripts/Defender.cs
+++ Assets/Scripts/Defense Scripts/Defender.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,7 +6,36 @@ using UnityEngine;
 public class Defender : MonoBehaviour
 {
     [SerializeField] private int _cost = 100;
+    [SerializeField] private float _buildDelay = 1f;
 
+    private void Start()
+    {
+        StartCoroutine(BuildDefense());
+    }
+
+    IEnumerator BuildDefense()
+    {
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(false);
+            foreach (Transform grandChild in child)
+            {
+                grandChild.gameObject.SetActive(false);
+            }
+        }
+
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(true);
+            yield return new WaitForSeconds(_buildDelay);
+
+            foreach (Transform grandChild in child)
+            {
+                grandChild.gameObject.SetActive(true);
+            }
+
+        }
+    }
 
     public bool Create(Defender defender, Vector3 position)
     {
